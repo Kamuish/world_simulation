fd4ecfdae4885a295ed3eb4b409644017fd5b52b
Kamuish <33263706+Kamuish@users.noreply.github.com>
2019-02-18 00:15:50 +0000

    pass time

--- world_simulation/Map/create_map.py
+++ world_simulation/Map/create_map.py
@@ -11,7 +11,19 @@ class Map():
 
 				self.cells[i,j] = plate
 
-		print(self.cells.shape)
+	def pass_time(self):
+		"""
+		Passes time for all cells
+			- Have to find a better way to do this -> IT'S UGLY
+		"""
+		for j in range(self.cells.shape[0]):
+			for i in range(self.cells.shape[0]):
+			
+				self.cells[i,j].pass_time()
+
+	def get_cell(self, x,y):
+		return self.cells[x,y]
+
 	def place_animal(self, xx,yy, animal):
 		self.cells[xx,yy].new_animal(animal)
 
