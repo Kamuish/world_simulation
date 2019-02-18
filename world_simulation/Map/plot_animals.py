c6235cb57aa9ef143bb7d02ee6c6d2f7b7e9b856
Kamuish <33263706+Kamuish@users.noreply.github.com>
2019-02-18 00:15:31 +0000

    Update plot_animals.py

--- world_simulation/Map/plot_animals.py
+++ world_simulation/Map/plot_animals.py
@@ -1,13 +1,19 @@
 from matplotlib import pyplot as plt 
 import numpy as np 
 
-def plot_carnivores(Map):
+def plot_carnivores(Map, which):
 	positions = np.zeros([Map.cells.shape[0],Map.cells.shape[0]])
 	print(positions.shape)
-	for j in range(Map.cells.shape[0]):
+	for j in range(Map.cells.shape[0]): # very inneficient 
 		for i in range(Map.cells.shape[0]):
-			positions[i,j] = Map.cells[i,j].get_carnivores()
+			positions[i,j] = Map.cells[i,j].get_carnivores(which)
+
 
 	fig, ax = plt.subplots()
-	img = ax.pcolor(positions)
+	plt.title("Carnivores: {}".format(which))
+	if which == 'alive':
+		c = 'Greens'
+	else:
+		c = 'OrRd'
+	img = ax.pcolor(positions, cmap = c)
 	plt.show()
