46c1570d052da0af4bcdf1db662c0d48cbb3a45a
Kamuish <33263706+Kamuish@users.noreply.github.com>
2019-02-18 00:32:23 +0000

    Create Herbivore_animal.py

---
+++ world_simulation/elements/Herbivore_animal.py
@@ -0,0 +1,22 @@
+from .base_elements import Animal
+
+class Carnivore(Animal):
+	def __init__(self, health,sex,stamina,speed,hunger_tick, thirst_tick,fov_radius):
+		super().__init__(health, sex, stamina,speed,hunger_tick, thirst_tick,fov_radius)
+
+		self.type = 'Herbivore'
+
+
+	def search_for_destiny(self):
+
+	
+
+if __name__ == '__main__':
+	x = Carnivore(10,1,1,1,10,1)
+	x.pass_time()
+	print(x)
+	x.pass_time()
+	x.take_dmg(2)
+	print(x)
+	x.eat(30)
+	print(x)
