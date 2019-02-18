a39bdd1921c4ca4f5703de3292d5f24b16caf656
Kamuish <33263706+Kamuish@users.noreply.github.com>
2019-02-18 00:15:41 +0000

    pass time

--- world_simulation/Map/Tiles.py
+++ world_simulation/Map/Tiles.py
@@ -39,27 +39,50 @@ class Tile():
 		self.plants.append(plant)
 
 
-	def get_carnivores(self):
+	def get_carnivores(self, which):
 		count = 0
-		for j in self.animals:
+
+		if which == 'alive':
+			animals_to_search = self.animals
+		else:
+			animals_to_search = self.dead_animals
+		for j in animals_to_search:
 			if j.animal_type == 'Carnivore':
 				count +=1 
 		return count
 
+	def pass_time(self):
+		remove = [] # going to store the indexes to remove
+		for i in range(len(self.animals)):
+			self.animals[i].pass_time()
+			if not self.animals[i].is_alive:
+				remove.append(i)
+				self.dead_animals.append(self.animals[i])
+
+		for rm in remove:
+			self.animals.pop(rm)
+		for dead_animal in self.dead_animals: # implement decay of the body
+			dead_animal.pass_time()	
+
+	@property
+	def live_creatures(self):
+		return self.animals
+
+	@property
+	def dead_creatures(self):
+		return self.dead_animals
+	
 	@property
 	def total_animals(self):		
 		return len(self.animals)
+
+	
 		
 	def __repr__(self):
-		return "Tile in position {} with {} animals and {} plants".format(self.coords, len(self.animals), len(self.plants))
+		return "Tile in position {} with {} live animals and {} plants".format(self.coords, len(self.animals), len(self.plants))
 
 
-	def pass_time(self):
-		for animal in self.animals:
-			animal.pass_time()
 
-		for dead_animal in self.dead_animals: # implement decay of the body
-			dead_animal.pass_time()	
 			
 class Water_Tile(Tile):
 	def __init__(self):
