36bb83cae664280e2291deff7fbdb2b5b170e296
Kamuish <33263706+Kamuish@users.noreply.github.com>
2019-02-18 00:48:43 +0000

    global plots

--- world_simulation/main.py
+++ world_simulation/main.py
@@ -1,12 +1,18 @@
 from elements import Carnivore
 
-from Map import Map
+from Map import Map, plot_carnivores, plot_all
 
 def main():
 	game_map = Map(4)
+	aa = Carnivore(health = 1, sex = 'Male', stamina = 1,speed = 1,hunger_tick = 101, thirst_tick = 1, fov_radius = 1)
 
-	aa = Carnivore(1,1,1,1,1,1)
 	game_map.place_animal(1,1,aa)
+	game_map.place_animal(2,2,aa)
 
-	print(game_map.cells[1,1].animals[0])
+	plot_all(game_map)
+
+	game_map.pass_time()
+
+	plot_all(game_map, which = 'dead')
+	
 main()
